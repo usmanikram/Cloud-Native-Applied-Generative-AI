f8ff4594ffe9ee0197df10a2997ae54575aeacee
Usman <usmanikram.ch@gmail.com>
2024-11-14 17:50:41 +0500

    Assignment #1

---
+++ Assignments/Assignment1/A1.py
@@ -0,0 +1,43 @@
+#Name: Muhammad Usman Ikram
+#Roll#: PIAIC247758
+
+# 1.Basic Printing
+print("Hello World")
+print("Usman")
+
+
+# 2. Printing Multiple Items
+print("Muhammad Usman " + "Ikram")
+print("1 "+ "2 " + "3")
+
+
+# 3. Printing Special Characters:
+print("Hello\nWorld")
+print("Hello\tWorld")
+
+
+# 4. Using the sep Parameter:
+print("apple", "banana" , "cherry", sep=',')
+print("1","2","3",sep='-')
+
+
+# 5. Using the end Parameter:
+print("Hello",end=' ')
+print("World")
+print("1",end='')
+print("2")
+
+
+# 6. Escape Characters:
+print("He said, \"Hello!\"")
+print("This is a backslash: \\")
+
+
+# 7. Combining Text and Numbers:
+print("I'm 28 years old.")
+print("This is number " + str(5))
+
+
+# 8. Basic Loop for Printing:
+for i in range(5):
+    print(i+1)
