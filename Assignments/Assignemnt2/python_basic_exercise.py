0333801c608064a040ee1fad7771595dc7caff54
Usman <usmanikram.ch@gmail.com>
2024-11-15 12:30:20 +0500

    Assignment #2

---
+++ Assignments/Assignemnt2/python_basic_exercise.py
@@ -0,0 +1,102 @@
+#Python Basic Exercies
+
+
+#1
+message = "Hello World!"
+print(message)
+
+#2
+message = "Hello World!"
+print(message)
+message = "Hello Universe!"
+print(message)
+
+
+#3
+name = "Kashif"
+message = "Hello {}, would you like to learn some Python today?"
+print(message.format(name))
+
+#4
+name = "ahMad"
+print(name.upper())
+print(name.lower())
+print(name.title())
+
+#5
+quote = "Nearly all men can stand adversity, but if you want to test a man’s character, give him power."
+author_fname="abraham"
+author_lname="lincoln"
+
+print(author_fname.title() + " " + author_lname.title() + " once said, \" " +quote + "\"")
+
+
+#6 
+famous_person = "abraham lincoln"
+quote = "Nearly all men can stand adversity, but if you want to test a man’s character, give him power."
+
+print(famous_person.title() + " once said, \" " +quote + "\"")
+
+#8
+x=5
+y=10
+z=15
+print(x+y+z)
+
+
+#9
+a = 5
+b = 10
+print(a)
+print(b)
+b = b-a
+a = a+b
+print(a)
+print(b)
+
+
+#10
+purple = "purple"
+print(purple)
+
+
+#11
+pet_name = "Buddy"
+print(pet_name)
+pet_name = "Max"
+print(pet_name)
+
+#12
+Sunshine = "Sunshine"
+print(Sunshine)
+
+
+#13
+score = 100
+print(score)
+score =50
+print(score)
+
+#14
+city = "Lahore"
+print(city)
+
+#15
+text = "python programming"
+print(text.title())
+
+#16
+message = "HeLLo ! HoW arE YoU ?"
+print(message.lower())
+
+#17
+message = "HeLLo ! HoW arE YoU ?"
+print(message.upper())
+
+#18
+temperature = 25
+print("The current temperature is {} degrees.".format(temperature))
+
+#19
+poem = "Twinkle twinkle little star.\nHow I wonder what you are. \nUp above the world so high. \nLike a diamond in the sky."
+print(poem)
